a7b91a475d6d1cd81f8d5b1536d6de66c0ad2b57
Pheonix0101 <purushottam5252@gmail.com>
2024-04-04 21:00:13 +0530

    initial commit

---
+++ lib/homePage.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final TextEditingController _nameController = TextEditingController();
+  var result;
+
+  predictGender(String name) async {
+    var url = "https://api.genderize.io/?name=$name";
+    var response = await http.get(Uri.parse(url));
+    var data = jsonDecode(response.body);
+    result = "Gender: ${data['gender']}";
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Row(
+          mainAxisAlignment:
+              MainAxisAlignment.center, // Align children to start and end
+          children: [
+            Text('Gender predictor'), // Title aligned to the left
+          ],
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            const Text('enter name to predict its gender'),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                controller: _nameController,
+                decoration: const InputDecoration(
+                  hintText: 'Enter a name',
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () => predictGender(_nameController.text),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                  (Set<MaterialState> states) {
+                    if (states.contains(MaterialState.pressed))
+                      return const Color.fromARGB(255, 4, 117, 7); // Color when the button is pressed
+                    return Colors.pink; // Default color
+                  },
+                ),
+              ),
+              child: const Text('predict'),
+            ),
+            if (result != null) Text(result),
+          ],
+        ),
+      ),
+    );
+  }
+}
